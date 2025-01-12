b03e72b206620b10525f7fe8bfafbae3234b1129
Niamh Ferns <Niamh.K.Ferns@gmail.com>
2025-01-12 14:13:58 +1300

    TagTools created with functionality for searching completed and tested.

--- tag_tools.py
+++ tag_tools.py
@@ -1,13 +1,68 @@
 import json
+import os
+import platform
+import re
+
+CONST_DIRLIM = "/"
+if platform.system() == "windows":
+    CONST_DIRLIM = "\\"
+
+
+class Tag:
+    def __init__(self, json, id=None, alias=False):
+        if not id:
+            id = json["id"]
+        self.parent_id = json["id"]
+        self.id = id
+        self.is_alias = alias
+        self.translations = json["translations"]
+
+        if self.is_alias:
+            self.localisation = json["aliases"][self.id]["localisation"]
+            self.name = json["aliases"][self.id][self.localisation]
+            return
+
+        self.localisation = json["defaultLocalisation"]
+        self.name = json["translations"][self.localisation]
 
 
 class TagTools:
-    tags = {}
-
-    @staticmethod
-    def find(cls):
-        pass
+    tags = []
 
     @classmethod
-    def reload(cls)
-        pass
+    def find(cls, t):
+        for tag in cls.tags:
+            # Check against translations
+            for localisation, translation in tag["translations"].items():
+                if translation.lower() == t.lower():
+                    return Tag(tag)
+
+            # Check against aliases
+            # This is fucking gross...
+            for id, alias in tag["aliases"].items():
+                for key, value in alias.items():
+                    if key == "localisation" or key == "trueAlias":
+                        continue
+
+                    if value.lower() == t.lower():
+                        if alias["trueAlias"]:
+                            return Tag(tag, id=id, alias=True)
+                        return Tag(tag)
+
+            # Check against alternate spellings
+            for alternate_spelling in tag["alternateSpellings"]:
+                if bool(re.match(alternate_spelling["regex"], t)):
+                    target = alternate_spelling["target"]
+
+                    if target == "alias" and tag["aliases"][alternate_spelling["id"]]["trueAlias"]:
+                        return Tag(tag, id=alternate_spelling["id"], alias=True)
+                    return Tag(tag)
+
+    @classmethod
+    def reload(cls):
+        with open(f"{os.path.dirname(os.path.abspath(__file__))}{CONST_DIRLIM}tags.json", "r", encoding="utf-8") as file:
+            cls.tags = json.load(file)
+
+        for tag in cls.tags:
+            for spelling in tag["alternateSpellings"]:
+                spelling["regex"] = re.compile(r'^(' + spelling["regex"] + ')$', re.IGNORECASE)
---
+++ test_tag_tools.py
@@ -0,0 +1,166 @@
+import unittest
+from tag_tools import TagTools
+
+
+class TestTagTools(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        TagTools.reload()
+
+    def test_find_translations(self):
+        query = "1-panel"
+        expected_id = "GHthFcCppABKx-okOMBFM"
+        expected_parent = "GHthFcCppABKx-okOMBFM"
+
+        result = TagTools.find(query)
+        fail_msg = f"Query '{query}' expected '{expected_parent}:{expected_id}', found '{result.parent_id}:{result.id}'"
+        self.assertEqual(
+            result.id,
+            expected_id,
+            fail_msg
+        )
+        self.assertEqual(
+            result.parent_id,
+            expected_parent,
+            fail_msg
+        )
+        print(f"{query} |\n Expected: {expected_parent}:{expected_id} - Found: {result.parent_id}:{result.id}")
+
+    def test_find_translations_1(self):
+        query = "yandere"
+        expected_id = "Aqfs4DtaDQFei0vx9d9yl"
+        expected_parent = "Aqfs4DtaDQFei0vx9d9yl"
+
+        result = TagTools.find(query)
+        fail_msg = f"Query '{query}' expected '{expected_parent}:{expected_id}', found '{result.parent_id}:{result.id}'"
+        self.assertEqual(
+            result.id,
+            expected_id,
+            fail_msg
+        )
+        self.assertEqual(
+            result.parent_id,
+            expected_parent,
+            fail_msg
+        )
+        print(f"{query} |\n Expected: {expected_parent}:{expected_id} - Found: {result.parent_id}:{result.id}")
+
+    def test_find_translations_2(self):
+        query = "ThisIsNotARealTag"
+        expected_id = None
+        expected_parent = None
+
+        result = TagTools.find(query)
+        fail_msg = ""
+        if result:
+            fail_msg = f"Query '{query}' expected '{expected_parent}:{expected_id}', found '{result.parent_id}:{result.id}'"
+        self.assertEqual(
+            result,
+            expected_id,
+            fail_msg
+        )
+        self.assertEqual(
+            result,
+            expected_parent,
+            fail_msg
+        )
+        print(f"{query} |\n Expected: {None}:{None} - Found: {result}:{result}")
+
+    def test_find_aliases(self):
+        query = "yakuza"
+        expected_id = "8qW02GELImGk9MKez0Ry2"
+        expected_parent = "X0S2qdOkxLC6NLACXUfcX"
+
+        result = TagTools.find(query)
+        fail_msg = f"Query '{query}' expected '{expected_parent}:{expected_id}', found '{result.parent_id}:{result.id}'"
+        self.assertEqual(
+            result.id,
+            expected_id,
+            fail_msg
+        )
+        self.assertEqual(
+            result.parent_id,
+            expected_parent,
+            fail_msg
+        )
+        print(f"{query} |\n Expected: {expected_parent}:{expected_id} - Found: {result.parent_id}:{result.id}")
+
+    def test_find_aliases_1(self):
+        query = "school band"
+        expected_id = "s_1S5lxsbr4KIHXn7Pkhr"
+        expected_parent = "s_1S5lxsbr4KIHXn7Pkhr"
+
+        result = TagTools.find(query)
+        fail_msg = f"Query '{query}' expected '{expected_parent}:{expected_id}', found '{result.parent_id}:{result.id}'"
+        self.assertEqual(
+            result.id,
+            expected_id,
+            fail_msg
+        )
+        self.assertEqual(
+            result.parent_id,
+            expected_parent,
+            fail_msg
+        )
+        print(f"{query} |\n Expected: {expected_parent}:{expected_id} - Found: {result.parent_id}:{result.id}")
+
+    def test_find_alternate_spellings(self):
+        query = "basket-ball"
+        expected_id = "Z0RmnbuHQJHb3YXADgIJ4"
+        expected_parent = "KMrJRGscw5qLwlcDIzI8U"
+
+        result = TagTools.find(query)
+        fail_msg = f"Query '{query}' expected '{expected_parent}:{expected_id}', found '{result.parent_id}:{result.id}'"
+        self.assertEqual(
+            result.id,
+            expected_id,
+            fail_msg
+        )
+        self.assertEqual(
+            result.parent_id,
+            expected_parent,
+            fail_msg
+        )
+        print(f"{query} |\n Expected: {expected_parent}:{expected_id} - Found: {result.parent_id}:{result.id}")
+
+    def test_find_alternate_spellings_1(self):
+        query = "school-band"
+        expected_id = "s_1S5lxsbr4KIHXn7Pkhr"
+        expected_parent = "s_1S5lxsbr4KIHXn7Pkhr"
+
+        result = TagTools.find(query)
+        fail_msg = f"Query '{query}' expected '{expected_parent}:{expected_id}', found '{result.parent_id}:{result.id}'"
+        self.assertEqual(
+            result.id,
+            expected_id,
+            fail_msg
+        )
+        self.assertEqual(
+            result.parent_id,
+            expected_parent,
+            fail_msg
+        )
+        print(f"{query} |\n Expected: {expected_parent}:{expected_id} - Found: {result.parent_id}:{result.id}")
+
+    def test_find_alternate_spellings_2(self):
+        query = "schools"
+        expected_id = "HEoNGy9TTQSZZoWqDsMcd"
+        expected_parent = "HEoNGy9TTQSZZoWqDsMcd"
+
+        result = TagTools.find(query)
+        fail_msg = f"Query '{query}' expected '{expected_parent}:{expected_id}', found '{result.parent_id}:{result.id}'"
+        self.assertEqual(
+            result.id,
+            expected_id,
+            fail_msg
+        )
+        self.assertEqual(
+            result.parent_id,
+            expected_parent,
+            fail_msg
+        )
+        print(f"{query} |\n Expected: {expected_parent}:{expected_id} - Found: {result.parent_id}:{result.id}")
+
+
+if __name__ == "__main__":
+    unittest.main()
